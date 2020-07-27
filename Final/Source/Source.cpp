c9b3d7eb0c5c032d59e455949717529c942d9526
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2020-07-27 15:16:49 +0300

    Backend

---
+++ Final/Source/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "../Backend/Graphics.h"
+#include "../Backend/Border/Border.h"
+#include "../Backend/EventEngine.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+
+	EventEngine e;
+
+
+}
